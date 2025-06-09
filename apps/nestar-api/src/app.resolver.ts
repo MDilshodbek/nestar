0bbd0eb02e9b45541ccf0a52f99034bcfeb9f289
MDilshodbek <mdbek55@gmail.com>
2025-06-09 20:25:58 +0900

    feat: integrate validation pipes logic

--- apps/nestar-api/src/app.resolver.ts
+++ apps/nestar-api/src/app.resolver.ts
@@ -5,6 +5,6 @@ import { Query, Resolver } from '@nestjs/graphql';
 export class AppResolver {
     @Query(() => String)
     public sayHello(): string {
-        return "This app servic"
+        return "GraphQL API Server"
     }
 }
