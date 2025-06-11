29a249b9318017115d142f0a7d75e12c0c2d645b
MDilshodbek <mdbek55@gmail.com>
2025-06-11 22:17:50 +0900

    feat: done task - ZL

--- train.ts
+++ train.ts
@@ -14,11 +14,19 @@
 
 // printNumbers();
 
-console.log('Task - ZM');
+// console.log('Task - ZM');
 
-function reverseInteger(num: number): number {
-	const reversedStr = num.toString().split('').reverse().join('');
-	return parseInt(reversedStr, 10);
-}
+// function reverseInteger(num: number): number {
+// 	const reversedStr = num.toString().split('').reverse().join('');
+// 	return parseInt(reversedStr, 10);
+// }
 
-console.log(reverseInteger(123456789));
+// console.log(reverseInteger(123456789));
+
+console.log('Task - ZL');
+
+const stringToKebab = (input: string): string => {
+	return input.trim().toLowerCase().replace(/\s+/g, '-');
+};
+
+console.log(stringToKebab('I Love Kebab'));
