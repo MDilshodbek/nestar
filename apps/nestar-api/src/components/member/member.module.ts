8d169b52d971c14692c50e99f71ca804520ad028
MDilshodbek <mdbek55@gmail.com>
2025-06-20 20:24:31 +0900

    feat: modification of property module

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -5,9 +5,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
+import { PropertyModule } from '../property/property.module';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]), AuthModule, ViewModule],
+	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]), AuthModule, ViewModule, PropertyModule],
 	providers: [MemberResolver, MemberService],
 })
 export class MemberModule {}
