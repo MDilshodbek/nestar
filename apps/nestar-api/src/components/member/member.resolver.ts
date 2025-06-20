ff9d7bf0c9bc506d8d901f8ead2341cdb5c03607
MDilshodbek <mdbek55@gmail.com>
2025-06-20 17:50:38 +0900

    feat:

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -46,7 +46,7 @@ export class MemberResolver {
 	@Query(() => String)
 	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
 		console.log('Query: checkAuth');
-		return `Hi ${authMember.memberFullName}, you are ${authMember.memberType}, your phone number is ${authMember.memberPhone} and your id is ${authMember._id}`;
+		return `Hi ${authMember.memberNick}, you are ${authMember.memberType}, your phone number is ${authMember.memberPhone} and your id is ${authMember._id}`;
 	}
 
 	@UseGuards(WithoutGuard)
