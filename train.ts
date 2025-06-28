ff4afeabc6b52cea39ad76dff747ff80f694138c
MDilshodbek <mdbek55@gmail.com>
2025-06-29 00:07:09 +0900

    feat: done Task - ZS

--- train.ts
+++ train.ts
@@ -116,21 +116,39 @@
 
 // console.log(findDuplicates([1, 2, 3, 4, 5, 4, 3, 4]));
 
-console.log('Task - ZR');
+// console.log('Task - ZR');
 
-const countNumberAndLetters = (input: string): { number: number; letter: number } => {
-	let number = 0;
-	let letter = 0;
+// const countNumberAndLetters = (input: string): { number: number; letter: number } => {
+// 	let number = 0;
+// 	let letter = 0;
 
-	for (const char of input) {
-		if (/[0-9]/.test(char)) {
-			number++;
-		} else if (/[a-zA-Z]/.test(char)) {
-			letter++;
-		}
+// 	for (const char of input) {
+// 		if (/[0-9]/.test(char)) {
+// 			number++;
+// 		} else if (/[a-zA-Z]/.test(char)) {
+// 			letter++;
+// 		}
+// 	}
+
+// 	return { number, letter };
+// };
+
+// console.log(countNumberAndLetters('string152%\\¥'));
+
+console.log('Task - ZS');
+
+const singleNumber = (arr: number[]): number | null => {
+	const map = new Map<number, number>();
+
+	for (const num of arr) {
+		map.set(num, (map.get(num) ?? 0) + 1);
 	}
 
-	return { number, letter };
+	for (const [key, value] of map.entries()) {
+		if (value === 1) return key;
+	}
+
+	return null;
 };
 
-console.log(countNumberAndLetters('string152%\\¥'));
+console.log(singleNumber([4, 2, 1, 2, 1]));
